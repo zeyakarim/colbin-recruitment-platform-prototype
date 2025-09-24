cf5797d9ea5ed82b79f5f82ef144dbae7048581a
zeyakarim <zeyakarim79@gmail.com>
2025-09-24 22:14:54 +0530

    [Feat] Add Analytics

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,13 +3,15 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { Analytics } from '@vercel/analytics/react';
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-    <React.StrictMode>
-        <QueryClientProvider client={queryClient}>
-            <App />
-        </QueryClientProvider>
-    </React.StrictMode>
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <App />
+      <Analytics />
+    </QueryClientProvider>
+  </React.StrictMode>
 );
